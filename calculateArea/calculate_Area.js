6572816102acf0731f6f0854d7aea8df21e4ee95
hastrotech1 <hastrotech1@gmail.com>
2025-08-05 13:15:06 -0400

    completed

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,9 +2,23 @@ let len;
 let width;
 
 function calculateArea(){
-    len = parseFloat(document.getElementById("lenght").value);
+    len = parseFloat(document.getElementById("len").value);
     width = parseFloat(document.getElementById("width").value);
+
+    let area = len * width;
     document.getElementById("result").innerHTML = `The area of the rectangle is: ${area}`;
 }
 
-let area = len * width;
+let grocery1;
+let grocery2;
+let grocery3;
+
+function groceryTracker(){
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+    let totalAmout = grocery1 + grocery2 + grocery3;
+
+    document.getElementById("total-amount").innerHTML = `The total amount for the groceries is: ${totalAmout}`;
+}
